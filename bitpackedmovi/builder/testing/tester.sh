ba890734d9933f836e4c825bad60bf40826e6b4b
asanaullah2015 <asanaullah2015@fau.edu>
2025-09-02 14:02:15 -0400

    More scripting

--- bitpackedmovi/builder/testing/tester.sh
+++ bitpackedmovi/builder/testing/tester.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then 
-    echo "please specify exactly 3 command line arguments: builder1, builder2, and generator"
+if [ $# -ne 4 ]; then 
+    echo "please specify exactly 3 command line arguments: ropebwt3, builder1, builder2, and generator"
     exit 1
 fi
 
-echo $3 $2 $1
+mkdir -p generated
+mkdir -p temp
+mkdir -p bwt
+
+$4 > generated/stringsAndReverse
+awk 'NR%2 == 1' generated/stringsAndReverse > generated/strings    
+
+$3 generated/stringsAndReverse > bwt/a_kactl
+
+$1 build -L -d -o temp/a generated/strings
+$2 temp/a > bwt/a_ours
+
+echo $4 $3 $2 $1
