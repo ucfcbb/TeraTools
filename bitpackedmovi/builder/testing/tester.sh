d75830463091511e597c9cd340ff4a852f9f9666
asanaullah2015 <asanaullah2015@fau.edu>
2025-09-03 14:46:31 -0400

    Testing developments

--- bitpackedmovi/builder/testing/tester.sh
+++ bitpackedmovi/builder/testing/tester.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+set -e
+
 if [ $# -ne 4 ]; then 
     echo "please specify exactly 3 command line arguments: ropebwt3, builder1, builder2, and generator"
     exit 1
@@ -9,12 +12,19 @@ mkdir -p generated
 mkdir -p temp
 mkdir -p bwt
 
-$4 > generated/stringsAndReverse
+$4 > generated/stringsAndReverseRaw
+tail -n +3 generated/stringsAndReverseRaw > generated/stringsAndReverse
+#tail -n +3 generated/stringsAndReverseRaw > generated/stringsAndReverseMetadata
+#head -n -1 generated/stringsAndReverseMetadata > generated/stringsAndReverse
+
 awk 'NR%2 == 1' generated/stringsAndReverse > generated/strings    
 
-$3 generated/stringsAndReverse > bwt/a_kactl
+head -n 2 generated/stringsAndReverseRaw > generated/stringsAndReverseMetadata
+#tail -n 1 generated/stringsAndReverseRaw >> generated/stringsAndReverseMetadata
+
+cat <(wc -l < generated/stringsAndReverse) generated/stringsAndReverse | $3
 
 $1 build -L -d -o temp/a generated/strings
-$2 temp/a > bwt/a_ours
+$2 temp/a temp/a > bwt/a_ours
 
 echo $4 $3 $2 $1
