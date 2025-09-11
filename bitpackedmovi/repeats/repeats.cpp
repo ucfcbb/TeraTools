daa5d4b61c7d58ddc0422589b947aa5e64c23161
asanaullah2015 <asanaullah2015@fau.edu>
2025-09-11 09:02:24 -0400

    Added repeats option to repeats.cpp

--- bitpackedmovi/repeats/repeats.cpp
+++ bitpackedmovi/repeats/repeats.cpp
@@ -2,24 +2,40 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 2 && argc != 3) {
-        std::cerr << "Usage: repeats <input.optbwtrl> [Length Threshold]\n"
+    if (argc != 3 && argc != 4) {
+        std::cerr << "Usage: repeats {SM,LM} [Length Threshold] <input.optbwtrl>\n"
             << argc-1 << " arguments passed instead of 1 or 2" << std::endl;
         exit(1);
     }
-    
+    if (strcmp(argv[1], "SM") && strcmp(argv[1], "LM")) {
+        std::cerr << "First argument must be either \"SM\" or \"LM\"."
+           << " If passed SM, program will output supermaximal repeats."
+           << " If passed LM, it will output regular (a.k.a. maximal a.k.a. locally maximal) repeats." << std::endl;
+        exit(1);
+    }
     uint64_t len = 1;
-    if (argc == 3)
+    if (argc == 4)
         len = atoi(argv[2]);
-    std::cout << "Outputting supermaximal repeats of length " << len << std::endl;
+    std::cout << "Outputting" << ((strcmp(argv[1], "LM"))? " supermaximal repeats" : " repeats")
+        << " of length " << len << std::endl;
+
+    
 
 
     Timer.start("Loading");
-    OptBWTRL index(argv[1]);
+    OptBWTRL index(argv[argc-1]);
     Timer.stop(); //Loading
 
-    Timer.start("Computing Supermaximal Repeats");
-    index.superMaximalRepeats(std::cout, len);
-    Timer.stop();
+    if (strcmp(argv[1], "SM") == 0) {
+        Timer.start("Computing Supermaximal Repeats");
+        index.superMaximalRepeats(std::cout, len);
+        Timer.stop();
+    }
+
+    if (strcmp(argv[1], "LM") == 0) {
+        Timer.start("Computing repeats");
+        index.repeats(std::cout, len);
+        Timer.stop();
+    }
     return 0;
 }
