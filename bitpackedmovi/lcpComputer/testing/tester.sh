eaa1b4a3c2127a96e4c4c2cf89ebe66ca57060a9
asanaullah2015 <asanaullah2015@fau.edu>
2025-10-08 15:12:53 -0400

    Added tester for min lcp computation per run

---
+++ bitpackedmovi/lcpComputer/testing/tester.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ $# -ne 8 ]; then 
+    echo "please specify exactly 8 command line arguments: ropebwt3, BWTbuilder1, BWTbuilder2(kactl), lcpComputer1, lcpComputer2(kactl), generator, loader, and syngenparams"
+    exit 1
+fi
+
+echo "Making directories for temporary files"
+mkdir -p generated
+mkdir -p temp
+mkdir -p computedOutputs
+mkdir -p consoleOutputs
+
+
+echo "Generating input strings"
+$6 $8> generated/stringsAndReverseRaw
+head -n -1 generated/stringsAndReverseRaw > generated/stringsAndReverse
+awk 'NR%2 == 1' generated/stringsAndReverse > generated/strings    
+
+echo "Generating supporting indexes (ropebwt3 and optbwtrl)"
+$1 build -L -d -o temp/a generated/strings > consoleOutputs/ropebwt3
+$2 temp/a temp/a > consoleOutputs/builder
+
+echo "Generating BWTs"
+$7 BWT temp/a.optbwtrl > computedOutputs/rawBWT1
+$3 < generated/stringsAndReverse > computedOutputs/rawBWT2
+
+echo "Checking if BWTs match"
+diff computedOutputs/rawBWT1 computedOutputs/rawBWT2
+
+echo "Generating (offset, minLCP) pairs for every run by each method"
+$7 MINLCP temp/a.optbwtrl > computedOutputs/minlcpBuilder
+$4 temp/a computedOutputs/minlcpLCPcomputer > consoleOutputs/lcpComputer
+$5 < generated/stringsAndReverse > computedOutputs/minlcpKactl
+
+echo "Checking if minLCP pairs match" 
+diff computedOutputs/minlcpKactl computedOutputs/minlcpLCPcomputer.rlcp
+diff computedOutputs/minlcpBuilder computedOutputs/minlcpKactl
+
+echo "Cleaning up files"
+rm generated/stringsAndReverseRaw generated/stringsAndReverse generated/strings
+rm temp/a consoleOutputs/ropebwt3 temp/a.optbwtrl temp/a_StructTree.html
+rm computedOutputs/rawBWT1 computedOutputs/rawBWT2
+rm computedOutputs/minlcpLCPcomputer.optbwtrl computedOutputs/minlcpLCPcomputer_StructTree.html 
+rm computedOutputs/minlcpBuilder computedOutputs/minlcpLCPcomputer.rlcp computedOutputs/minlcpKactl
+rm consoleOutputs/lcpComputer consoleOutputs/builder construction.html 
