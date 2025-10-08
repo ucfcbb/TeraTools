9f2a553ddd4909b4ab35f124466cc2a0cb201014
asanaullah2015 <asanaullah2015@fau.edu>
2025-10-08 15:20:58 -0400

    Updated builder/testing

--- bitpackedmovi/builder/testing/tester.sh
+++ bitpackedmovi/builder/testing/tester.sh
@@ -3,8 +3,8 @@
 set -x
 set -e
 
-if [ $# -ne 5 ]; then 
-    echo "please specify exactly 5 command line arguments: ropebwt3, builder1, builder2, generator, and loader"
+if [ $# -ne 6 ]; then 
+    echo "please specify exactly 6 command line arguments: ropebwt3, builder1, builder2, generator, loader, and parameters for generator"
     exit 1
 fi
 
@@ -12,8 +12,8 @@ mkdir -p generated
 mkdir -p temp
 mkdir -p bwt
 
-$4 > generated/stringsAndReverseRaw
-head -n -2 generated/stringsAndReverseRaw > generated/stringsAndReverse
+$4 $6 > generated/stringsAndReverseRaw
+head -n -1 generated/stringsAndReverseRaw > generated/stringsAndReverse
 #tail -n +3 generated/stringsAndReverseRaw > generated/stringsAndReverseMetadata
 #head -n -1 generated/stringsAndReverseMetadata > generated/stringsAndReverse
 
@@ -28,7 +28,7 @@ $3 < generated/stringsAndReverse > temp/rawBWT2
 $1 build -L -d -o temp/a generated/strings
 $2 temp/a temp/a > bwt/a_ours
 
-$5 temp/a.optbwtrl > temp/rawBWT1
+$5 BWT temp/a.optbwtrl > temp/rawBWT1
 
 diff temp/rawBWT1 temp/rawBWT2
 
@@ -38,3 +38,7 @@ diff temp/rawBWT1 temp/rawBWT2
 # fi
 
 #echo $5 $4 $3 $2 $1
+
+rm generated/stringsAndReverseRaw generated/stringsAndReverse generated/strings generated/stringsAndReverseMetadata
+rm temp/{a,a.optbwtrl,a_StructTree.html} bwt/a_ours
+rm temp/rawBWT{1,2}
