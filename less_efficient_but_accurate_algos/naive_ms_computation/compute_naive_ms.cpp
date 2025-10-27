4fd9ae8dcd2db28dff65362b5dd5b379e630e07e
Pramesh Shakya <prmshakya@gmail.com>
2025-10-27 16:11:49 -0400

    Clean up and matching statistic (true definition of suffix that matches the longest) naive implementation start

--- less_efficient_but_accurate_algos/naive_ms_computation/compute_naive_ms.cpp
+++ less_efficient_but_accurate_algos/naive_ms_computation/compute_naive_ms.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <cassert>
 
 #include "build_bwt.h"
 
@@ -36,91 +37,14 @@ void readPatternFile(const string& patternFile, string& pattern){
 	return;
 }
 
-int main(int argc, char* argv[]) {
-	// input will be text
-	// input will be pattern/query
-	//
-	if (argc != 3) {
-		std::cout << "./compute_naive_ms <path/to/text/file> <path/to/pattern/file>" << std::endl;
-		exit(1);
-	}
-	unordered_map<char, int> BWTInt;
-	BWTInt['$'] = 0;
-	BWTInt['A'] = 1;
-	BWTInt['C'] = 2;
-	BWTInt['G'] = 3;
-	BWTInt['T'] = 4;
-	BWTInt['N'] = 5;
-
-	std::string textFile = argv[1];
-	std::string patternFile = argv[2];
-
-	std::cout << "Text file: " << textFile << std::endl;
-	std::cout << "Pattern file: " << patternFile << std::endl;
-
-    int numStrs = 0;
-    vector<int> str_starts;
-    string combined;
-	readTextFile(textFile, str_starts, combined, numStrs);
-
-	string pattern;
-	readPatternFile(patternFile, pattern);
-	cout << "Lenth of pattern: " << pattern.size() << endl;
-
-	std::cout << "numStrs = " << numStrs << std::endl;
-	std::cout << "combined = " << combined << std::endl;
-
-    map<char, int> id;
-    build_char_map(combined, numStrs, id);
-	for(auto kv: id) {
-		std::cout << kv.first << " : " << kv.second << std::endl;
-	}
-	std::cout << std::endl;
-
-    int n = combined.size();
-	cout << "n = " << n << endl;
-    vi num = build_num_vec(combined, id, numStrs);
-	// cout << "num.length = " << num.size() << endl;
-	// printVectors<int>(num, "num");
-
-	// Compute SA and LCP
-    SuffixArray sa(num, id.size() + numStrs + 1);
-	// printBWT(sa, combined);
-	vector<int> bwt(sa.sa.size(), 0);
-	compute_bwt(sa, combined, n, bwt);
-	print_bwt(sa, combined, n);
-
-	for(int i = 1; i <= bwt.size(); ++i){
-		cout << bwt[i] << " ";
-	}
-	cout << endl;
+pair<vector<int>, vector<int>> computePredOnlyMS(const string& pattern, vector<int>& bwt, vector<int>& LF, SuffixArray& sa, vector<int>& count, map<char, int>& id){
+	// Compute row and length
+	// Here, the definition of row is the predecessor suffix
 	
-	// Compute C array 
-	// the maximum value in the values of the key,value pair
-	vector<int> count(id.size() + numStrs + 2);
-	compute_c(num, count);
-	printVectors<int>(count, "count");
-
-	cout << "SuffixArray.length = " << sa.sa.size() << endl;
-	printVectors<int>(sa.sa, "Suffix Array");
-	cout << "LCP.length = " << sa.lcp.size() << endl;
-	printVectors<int>(sa.lcp, "LCP");
-
-    // Compute LF mapping
-    vector<int> rank(n);
-    for (int i = 1; i <= n; i++) rank[sa.sa[i]] = i - 1;
-    vector<int> LF(n);
-    for (int i = 0; i < n; i++) LF[i] = rank[(sa.sa[i + 1] - 1 + n) % n];
-	// cout << "Rank.length = " << rank.size() << endl;
-	// printVectors<int>(rank, "rank");
-	cout << "LF.length = " << LF.size() << endl;
-	printVectors<int>(LF, "LF");
-
-	// compute ms
 	vector<int> ms_row(pattern.size(), 0);
 	vector<int> ms_length(pattern.size(), 0);
-	// Compute row
-	int pos = 1; 
+
+	int pos = 1;  // starting at the top
 	int len = 0; 
 	for (int i = pattern.size()-1; i >= 0; --i) {
 		if (pos == 0 || pattern[i] != bwt[pos]) {
@@ -148,18 +72,177 @@ int main(int argc, char* argv[]) {
 		ms_row[i] = pos;
 		ms_length[i] = len;
 	}
+	return {ms_row, ms_length};
+}
 
-	cout << "MS.row: " << endl;
-	for(auto val: ms_row){
-		cout << val << " ";
-	}
-	cout << endl;
+void printUsage() {
+	std::cout << "Computes the matching statistics naively" << std::endl;
+	std::cout << "Usage:" << std::endl;
+	std::cout << "./compute_naive_ms <path/to/text/file> <path/to/pattern/file> [optional]\n" << std::endl;
+	std::cout << "If <pattern file> is not provided, just computes the BWT (and auxiliary data structures) of the text." << std::endl;
+	return;
+}
 
-	cout << "MS.suff: " << endl;
-	for(auto val: ms_row){
-		cout << sa.sa[val] << " ";
+int main(int argc, char* argv[]) {
+	// input will be text
+	// input will be pattern/query
+	
+	string textFile;
+	string patternFile;
+	bool computeMS = false;
+	switch (argc){
+		case 2:
+			textFile = argv[1];
+			break;
+		case 3:
+			textFile = argv[1];
+			patternFile = argv[2];
+			computeMS = true;
+			break;
+		default:
+			printUsage();
+			exit(1);
+			break;
 	}
-	cout << endl;
+
+	unordered_map<char, int> BWTInt;
+	BWTInt['$'] = 0;
+	BWTInt['A'] = 1;
+	BWTInt['C'] = 2;
+	BWTInt['G'] = 3;
+	BWTInt['T'] = 4;
+	BWTInt['N'] = 5;
+
+	// Read both text input files
+	cout << "Text file: " << textFile << endl;
+	int numStrs = 0;
+	vector<int> str_starts;
+	string combined;
+	readTextFile(textFile, str_starts, combined, numStrs);
+	cout << "combined/concatenated text = " << combined << endl;
+	cout << "numStrs = " << numStrs << endl;
+    map<char, int> id;
+    build_char_map(combined, numStrs, id);
+	// for(auto kv: id) {
+	// std::cout << kv.first << " : " << kv.second << std::endl;
+	// }
+	// std::cout << std::endl;
+    int n = combined.size();
+	cout << "n (length of concatenated text) = " << n << endl;
+    vi num = build_num_vec(combined, id, numStrs);
+	// cout << "num.length = " << num.size() << endl;
+	// printVectors<int>(num, "num");
+	// Compute SA and LCP
+    SuffixArray sa(num, id.size() + numStrs + 1);
+	// printBWT(sa, combined);
+	vector<int> bwt(sa.sa.size(), 0);
+	compute_bwt(sa, combined, n, bwt);
+	print_bwt(sa, combined, n);
+	// Compute C (count) array 
+	// the maximum value in the values of the key,value pair
+	vector<int> count(id.size() + numStrs + 2);
+	compute_c(num, count);
+	printVectors<int>(count, "count");
+
+	cout << "SuffixArray.length = " << sa.sa.size() << endl;
+	printVectors<int>(sa.sa, "Suffix Array");
+	cout << "LCP.length = " << sa.lcp.size() << endl;
+	printVectors<int>(sa.lcp, "LCP");
+
+    // Compute LF mapping
+    vector<int> rank(n);
+    for (int i = 1; i <= n; i++) rank[sa.sa[i]] = i - 1;
+    vector<int> LF(n);
+    for (int i = 0; i < n; i++) LF[i] = rank[(sa.sa[i + 1] - 1 + n) % n];
+	// cout << "Rank.length = " << rank.size() << endl;
+	// printVectors<int>(rank, "rank");
+	cout << "LF.length = " << LF.size() << endl;
+	printVectors<int>(LF, "LF");
+
+	if (computeMS) {
+		// Read pattern input files
+		std::cout << "Pattern file: " << patternFile << std::endl;
+		string pattern;
+		readPatternFile(patternFile, pattern);
+		cout << "pattern = " << pattern << std::endl;
+		cout << "Lenth of pattern: " << pattern.size() << endl;
+
+		// auto matchingStatistics = computePredOnlyMS(pattern, bwt, sa, count, id);
+
+		// Compute Matching Statistics
+		vector<int> ms_row(pattern.size(), 0);
+		vector<int> ms_length(pattern.size(), 0);
+		// int pos = 1; 
+		int pos = n;  // start at bottom so that it's easy to check with Moni's output as per Nate
+		int len = 0; 
+
+		for (int i = pattern.size()-1; i >= 0; --i) {
+			if (pos == 0 || pos == n + 1 || pattern[i] != bwt[pos]) {
+				// scan up until we find a character 
+				// that matches pattern[i]
+				int up = pos;
+				int upLen = len;
+				while (up > 0 && pattern[i] != bwt[up]) { 
+					upLen = min(sa.lcp[up], upLen);
+					--up;
+				}
+
+				// scan down until we find a character that matches pattern[i]
+				int down = pos;
+				int downLen = len;
+				while (down < n + 1 && pattern[i] != bwt[down]) { 
+					downLen = min(sa.lcp[down], downLen);
+					++down;
+				}
+
+				// whichever suffix has the maximal match
+				// that will be the new pos
+				// pos = up;
+				if (up == 0) {
+					pos = down;
+					len = downLen;
+				} else if (down == n + 1) {
+					pos = up;
+					len = upLen;
+				} else {
+					pos = (upLen >= downLen)?up:down;
+					len = max(upLen, downLen);
+				}
+			}
+
+			if (pos == 0 || pos == n + 1) {
+				// c[i] is the number of occurences of characters smaller than i that in the Text
+				cout << "We ended up here" << endl;
+				pos = count[id[pattern[i]]];
+				assert(len == 0);
+				len = 0;
+			} else {
+				pos = LF[pos-1] + 1;
+				++len;
+			}
+			ms_row[i] = pos;
+			ms_length[i] = len;
+		}
+
+		cout << "MS.row: " << endl;
+		for(auto val: ms_row){
+			cout << val << " ";
+		}
+		cout << endl;
+
+		cout << "MS.suff: " << endl;
+		for(auto val: ms_row){
+			cout << sa.sa[val] << " ";
+		}
+		cout << endl;
+
+		cout << "MS.len: " << endl;
+		for(auto val: ms_length) {
+			cout << val << " ";
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
 
