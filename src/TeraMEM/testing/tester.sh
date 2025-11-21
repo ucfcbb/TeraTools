15d079c4507e7134fdbbc8badbe2bf071a04f2c7
asanaullah2015 <asanaullah2015@fau.edu>
2025-11-21 10:48:54 -0500

    renamed MEM to TeraMEM

--- src/TeraMEM/testing/tester.sh
+++ src/TeraMEM/testing/tester.sh
@@ -5,14 +5,14 @@
 set -e
 
 if [[ $# -lt 9 ]]; then
-    echo "please specify exactly 9 command line arguments: ropebwt3, TeraLCP, msIndexBuilder, MEM, bruteForceRepeats, generator, syngenparams, mode:[SM,LM], lengthThreshold"
+    echo "please specify exactly 9 command line arguments: ropebwt3, TeraLCP, msIndexBuilder, TeraMEM, bruteForceRepeats, generator, syngenparams, mode:[SM,LM], lengthThreshold"
     exit 1
 fi
 
 ropebwt3=$1
 TeraLCP=$2
 msIndexBuilder=$3
-MEM=$4
+TeraMEM=$4
 MEM_BRUTE=$5
 generator=$6
 syngenparams=$7
@@ -39,7 +39,7 @@ awk 'NR%2 == 1' generated/stringsAndReverse > generated/strings
 echo "Generating supporting index (ropebwt3)"
 $ropebwt3 build -L -d -o temp/a.fmd generated/strings
 
-MEM_OUT="computedOutputs/MEM.out"
+TeraMEM_OUT="computedOutputs/TeraMEM.out"
 BRUTE_OUT="computedOutputs/bruteMEM.out"
 
 echo "Running TeraLCP on temp/a.fmd"
@@ -48,14 +48,14 @@ $TeraLCP -f fmd -i temp/a.fmd -t temp/temp -oindex temp/a -v quiet
 echo "Running msIndexBuilder on temp/a.lcp_index"
 $msIndexBuilder -i temp/a.lcp_index -o temp/a -v quiet
 
-echo "Running MEM on temp/a.ms_index"
-$MEM -MEM $MODE -i "temp/a.ms_index" -L $LEN -o $MEM_OUT -v quiet
+echo "Running TeraMEM on temp/a.ms_index"
+$TeraMEM -MEM $MODE -i "temp/a.ms_index" -L $LEN -o $TeraMEM_OUT -v quiet
 
 echo "Running brute_force_repeats on generated/stringsAndReverse"
 $MEM_BRUTE $MODE $LEN < generated/stringsAndReverse > $BRUTE_OUT
 
 
-header1=$(head -n 1 "$MEM_OUT")
+header1=$(head -n 1 "$TeraMEM_OUT")
 header2=$(head -n 1 "$BRUTE_OUT")
 
 if [[ "$header1" != "$header2" ]]; then
@@ -64,15 +64,15 @@ if [[ "$header1" != "$header2" ]]; then
     exit 1
 fi
 
-sort $MEM_OUT   -n -k1,1 -k2,2 -k3,3 -k4,4 > $MEM_OUT.sorted
+sort $TeraMEM_OUT   -n -k1,1 -k2,2 -k3,3 -k4,4 > $TeraMEM_OUT.sorted
 sort $BRUTE_OUT -n -k1,1 -k2,2 -k3,3 -k4,4 > $BRUTE_OUT.sorted
 
-diff -u $MEM_OUT.sorted $BRUTE_OUT.sorted
+diff -u $TeraMEM_OUT.sorted $BRUTE_OUT.sorted
 
 #echo "Success! Correct repeats output."
 
 echo "Cleaning up files"
 rm generated/stringsAndReverseRaw generated/stringsAndReverse generated/strings
 rm temp/a.fmd temp/a.lcp_index temp/temp construction.html temp/a.ms_index
-rm $MEM_OUT $BRUTE_OUT
-rm $MEM_OUT.sorted $BRUTE_OUT.sorted 
+rm $TeraMEM_OUT $BRUTE_OUT
+rm $TeraMEM_OUT.sorted $BRUTE_OUT.sorted 
